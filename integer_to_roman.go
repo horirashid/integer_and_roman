44ad88818bfde6c4919358868cd13dd8d4a4f635
horirashid <hawry.jawad@gmail.com>
2021-06-23 11:30:33 +0900

    first solution

--- integer_to_roman.go
+++ integer_to_roman.go
@@ -1,1 +1,24 @@
-package integer_and_roman
+package main
+import (
+	"fmt"
+)
+func int_to_roman(n int) string {
+	num := []int{1000,900,500,400,100,90,50,40,10,9,5,4,1}
+	roman :=[] string{"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"}
+	result := ""
+
+	for i:=0; i< len(num); i++ {
+		for num[i]<=n {
+			result += roman[i]
+			n -= num[i]
+
+		}
+	}
+	return result
+}
+
+func main() {
+	n := 36
+	r := int_to_roman(n)
+	fmt.Printf("%d in Roman letter is %s", n, r)
+}
